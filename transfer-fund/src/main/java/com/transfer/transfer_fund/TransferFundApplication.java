3962cef086dfb61c83218bed3f4bff63f24f7f19
PM113597 <PM113597@AESW42NLPC1456.ind.zensar.com>
2024-12-24 15:54:48 +0530

    Transfer funds module commit

---
+++ transfer-fund/src/main/java/com/transfer/transfer_fund/TransferFundApplication.java
@@ -0,0 +1,13 @@
+package com.transfer.transfer_fund;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TransferFundApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TransferFundApplication.class, args);
+	}
+
+}
---
+++ transfer-fund/src/test/java/com/transfer/transfer_fund/TransferFundApplicationTests.java
@@ -0,0 +1,13 @@
+package com.transfer.transfer_fund;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TransferFundApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
